0aaccf12fdda722093f2182b6c29fb557fd944be
sofiene-Ben <109277784+sofiene-Ben@users.noreply.github.com>
2023-08-09 16:40:52 +0200

    features : ajout mailer (#5)

---
+++ src/Form/DocumentShareType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class DocumentShareType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'adresse mail du destinataire',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('title', TextType::class, [
+                'disabled' => true,
+                'label' => 'document',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'ajoutez un message',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
